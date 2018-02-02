53b972297cc358ac8991f66514843ba0c4de74ef
Angus Malloch <angusbass@hotmail.com>
2018-02-02 12:11:18 +0000

    Song collection setup.

--- songs.rb
+++ songs.rb
@@ -4,9 +4,12 @@ class Songs
 def initialize(song_collection)
 
   @song_collections = [
-    rock: {name: "Wonderwall" artist: "Oasis"}
-    pop: { name: "Beat It" artist: "Michael Jackson"}
-    funk: { name: "September" artist: "Earth Wind and Fire"}
+    rock: { name: "Wonderwall", artist: "Oasis"},
+          { name: "Purple Haze", artist: "Jimi Hendrix"},
+    pop: { name: "Beat It", artist: "Michael Jackson"},
+         { name: "Spinning Around", artist: "Kylie Minogue"},
+    funk: { name: "September", artist: "Earth Wind and Fire"},
+          { name: "Superstition", artist: "Stevie Wonder"}
   ]
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -7,21 +7,24 @@ require_relative("../songs.rb")
 class Rooms_Test < MiniTest::Test
 #river
   def setup
-    song_col_rock = (@songs(song_collection)[:rock])
-    song_col_pop = (@songs(song_collection)[:pop])
-    song_col_funk = (@songs(song_collection)[:funk]
+    song_col_rock = @songs(song_collection)[:rock]
+    song_col_pop = @songs(song_collection)[:pop]
+    song_col_funk = @songs(song_collection)[:funk]
     room1 = ("Rock_room", 8, song_col_rock)
     room2 = ("Pop_room", 8, song_col_pop)
     room3 = ("Funk_room", 8, song_col_funk)
 
     @rooms = [ room1, room2, room3]
     @rooms Rooms.new(@rooms)
+    @songs Songs.new(song_collection)
 
   end
 
   #if room = "Rock room" play song_collection[:rock] ?
 
-  def
+  def test_rock_collection
+
+  end
 
 
 
